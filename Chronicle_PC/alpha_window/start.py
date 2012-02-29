5609da973179fe7b4f91f0dce583c28cad64a74d
Zach Fry <fryz@rpi.edu>
2012-02-29 17:00:16 -0500

    Trying to make PyQt work

--- Chronicle_PC/alpha_window/start.py
+++ Chronicle_PC/alpha_window/start.py
@@ -7,15 +7,16 @@ class StartQT4(QtGui.QMainWindow):
         QtGui.QWidget.__init__(self,parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-
         self.event_widgets = []
         QtCore.QObject.connect(self.ui.addEventButton, QtCore.SIGNAL("clicked()"), self.addEvent)
         
     def addEvent(self):
-        #width = self.ui.eventGrid.layoutHorizontalSpacing
-        #height = self.ui.eventGrid.layoutVerticalSpacing
+        cols = self.ui.eventGrid.columnCount()
+        rows = self.ui.eventGrid.rowCount()
+        eventindex = (int(len(self.event_widgets)/rows),len(self.event_widgets)%cols)
+
         new_event_widget = QtGui.QPushButton(self.ui.centralwidget)
-        new_event_widget.setGeometry(QtCore.QRect(0,0,20,20))
+        new_event_widget.setGeometry(self.ui.eventGrid.cellRect(eventindex[0], eventindex[1]))
         new_event_widget.setObjectName("tmpWidget"+str(len(self.event_widgets)+1))
         
         self.event_widgets.append(new_event_widget)
