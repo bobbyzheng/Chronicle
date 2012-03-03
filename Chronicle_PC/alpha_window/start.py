ae881263b8e86b19d6ea148dd1b2860d85191e8b
Zach Fry <fryz@rpi.edu>
2012-03-02 22:29:05 -0500

    Adding Funcitonality to Calendar API

--- Chronicle_PC/alpha_window/start.py
+++ Chronicle_PC/alpha_window/start.py
@@ -7,26 +7,22 @@ class StartQT4(QtGui.QMainWindow):
         QtGui.QWidget.__init__(self,parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.cols = 24
-        self.rows = 7
         self.event_widgets = []
-        self.setupCalendar(self)
-        QtCore.QObject.connect(self.ui.addEventButton, QtCore.SIGNAL("clicked()"), self.addEvent)
         
-    def setupCalendar(self):
-        for i in range(0,self.rows):
-            for j in range(0,self.cols):
-                new_event_widget = QtGui.QPushButton(self.ui.centralwidget)
-                new_event_widget.setGeometry(self.ui.eventGrid.cellRect(i,j))
-                new_event_widget.setObjectName("tmpWidget"+str(len(self.event_widgets)+1))
-                self.event_widgets.append(new_event_widget)
-                self.ui.eventGrid.addWidget(new_event_widget, i, j)
-       
+        for i in range(0,5):
+            self.ui.eventGrid.setRowMinimumHeight(i,30)
+            self.ui.eventGrid.setColumnMinimumWidth(i,30)
+            
+        for i in range(0,5):
+            for j in range(0,5):
+                tmp_btn = QtGui.QPushButton(self.ui.centralwidget)
+                tmp_btn.setObjectName("eventWidget"+str(len(self.event_widgets)))
+                QtCore.QObject.connect(tmp_btn, QtCore.SIGNAL("clicked()"), tmp_btn.hide)
+                self.ui.eventGrid.addWidget(tmp_btn, i,j)
+                self.event_widgets.append(tmp_btn)
+                
 
-
-    def addEvent(self):
-        
-
+            
 
 if __name__ == "__main__":
         app = QtGui.QApplication(sys.argv)
